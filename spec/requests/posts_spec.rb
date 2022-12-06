c2936527c0d66206560da4325ada548266fd6e25
Peter Njuguna <Ptahbn@gmail.com>
2022-12-06 11:37:32 +0300

    Tests for posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:user_id/posts/'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should include correct placeholder' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include('Here are the posts for user')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should include correct placeholder' do
+      get '/users/:user_id/posts/:id'
+      expect(response.body).to include('Here is the post for user')
     end
   end
 end
