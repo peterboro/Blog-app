2076c64ed4f6a39288afb45c879c71bd56c710e7
Peter Njuguna <Ptahbn@gmail.com>
2022-12-06 11:37:48 +0300

    Tests for user controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/index'
+      get '/users/'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'should include correct placeholder' do
+      get '/users/:id'
+      expect(response.body).to include('Here are the users')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      get '/users/:id'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should include correct placeholder' do
+      get '/users/:id'
+      expect(response.body).to include('Here is the user')
     end
   end
 end
