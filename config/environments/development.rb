d1a5133377e39ccc84683de71f5f83f1d2e939a8
edi-sipka@gmail.com <edisipka@gmail.com>
2022-12-13 16:37:09 +0100

    Add environment and letter openner gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  
   config.after_initialize do
     Bullet.enable        = true
     Bullet.alert         = true
